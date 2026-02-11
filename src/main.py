b36ce315eb79ca15b82b33d5772cecaa7c49ff62
prcio <dennisgarciadeleon@gmail.com>
2026-02-11 12:24:46 -0500

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,7 @@
+#Just writing down some of the variables
+
+res_time = 1            #sim resolution (s)
+r_rate = 20             #radar refresh rate (1/s)
+rad_range_est_sd = 5    #stan. dev of the range as measured by radar
+az_sd = 0.15*10^(-3)    #
+el_sd = 0.15*10^(-3)    #
